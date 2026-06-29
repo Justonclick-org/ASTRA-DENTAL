3f540daa292c2e93114a4c636894aa805ecd02ff
springupravi <ravi.pawar@springuplabs.com>
2026-06-29 23:55:55 +0530

    Updates

--- assets/IMG_1862-D0qsCh8y.js
+++
@@ -1,1 +0,0 @@
-const s="/ASTRA-DENTAL/assets/IMG_1860-Czd4t_oK.PNG",A="/ASTRA-DENTAL/assets/IMG_1861-B3zIMApo.PNG",t="/ASTRA-DENTAL/assets/IMG_1862-C4guZOO_.PNG";export{A as a,t as b,s};
--- assets/dr.amit-BqeiDpUh.js
+++
@@ -1,1 +0,0 @@
-const t="/ASTRA-DENTAL/assets/dr.amit-Dc_ZPnRH.PNG";export{t as d};
